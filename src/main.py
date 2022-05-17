66f5e8543b83fc313e33ed7edec47b48756feefe
prayge <sgjfxvs@gmail.com>
2022-05-17 05:29:06 +0100

    HippoOOP

--- src/main.py
+++ src/main.py
@@ -27,7 +27,7 @@ def main():
 
     data = MSD(
         task='Task04_Hippocampus',
-        batch_size=16,
+        batch_size=2,
         train_val_ratio=0.8,
     )
 
@@ -56,7 +56,51 @@ def main():
     )
     trainer = pl.Trainer(
         gpus=1,
+        accelerator="gpu",
         precision=16,
         callbacks=[early_stopping],
     )
     trainer.logger._default_hp_metric = False
+
+    start = datetime.now()
+    print('Training started at', start)
+    trainer.fit(model=model, datamodule=data)
+    print('Training duration:', datetime.now() - start)
+
+    model.to('cuda')
+    all_dices = []
+    get_dice = monai.metrics.DiceMetric(include_background=False, reduction='none')
+    with torch.no_grad():
+        for batch in data.val_dataloader():
+            inputs, targets = model.prepare_batch(batch)
+            logits = model.net(inputs.to(model.device))
+            labels = logits.argmax(dim=1)
+            labels_one_hot = torch.nn.functional.one_hot(labels).permute(0, 4, 1, 2, 3)
+            get_dice(labels_one_hot.to(model.device), targets.to(model.device))
+        metric = get_dice.aggregate()
+        get_dice.reset()
+        all_dices.append(metric)
+    all_dices = torch.cat(all_dices)
+
+    records = []
+
+    for ant, post in all_dices:
+        records.append({'Dice': ant, 'Label': 'Anterior'})
+        records.append({'Dice': post, 'Label': 'Posterior'})
+    df = pd.DataFrame.from_records(records)
+    ax = sns.stripplot(x='Label', y='Dice', data=df, size=10, alpha=0.5)
+    ax.set_title('Dice scores')
+
+    with torch.no_grad():
+        for batch in data.test_dataloader():
+            inputs = batch['image'][tio.DATA].to(model.device)
+            labels = model.net(inputs).argmax(dim=1, keepdim=True).cpu()
+            break
+    batch_subjects = tio.utils.get_subjects_from_batch(batch)
+    tio.utils.add_images_from_batch(batch_subjects, labels, tio.LabelMap)
+
+    for subject in batch_subjects:
+        subject.plot()
+
+if __name__ == '__main__':
+    main()
